1a6dbc75662c8c6e42d0e2c0d375794444b38da3
SansKarI2004 <ughadesanskar023@gmail.com>
2024-06-29 12:35:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import PassiveAggressiveRegressor
+
+data = pd.read_csv("./archive/Instagram data.csv", encoding = 'latin1')
+
+
+data.isnull().sum()
+
+data=data.dropna()
+
+data.info()
+
+
+#reach through followers
+plt.figure(figsize=(10, 8))
+plt.style.use('fivethirtyeight')
+plt.title("Distribution of Impressions From Home")
+#sns.distplot(data['From Home'])
+sns.histplot(data['From Home'])
+plt.show()
+
+#reach through hashtags
+plt.figure(figsize=(10, 8))
+plt.title("Distribution of Impressions From Hashtags")
+#sns.distplot(data['From Hashtags'])
+sns.histplot(data['From Hashtags'])
+plt.show()
+
+#reach through explore
+plt.figure(figsize=(10, 8))
+plt.title("Distribution of Impressions From Explore")
+#sns.distplot(data['From Explore'])
+sns.histplot(data['From Explore'])
+plt.show()
+
+#pie chart for reach through all sources
+home = data["From Home"].sum()
+hashtags = data["From Hashtags"].sum()
+explore = data["From Explore"].sum()
+other = data["From Other"].sum()
+
+labels = ['From Home','From Hashtags','From Explore','Other']
+values = [home, hashtags, explore, other]
+
+fig = px.pie(data, values=values, names=labels, 
+             title='Impressions on Instagram Posts From Various Sources', hole=0.5)
+fig.show()
+
+#analyzing caption content
+text = " ".join(i for i in data.Caption)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.style.use('classic')
+plt.figure( figsize=(12,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+# analyzing hashtags content
+text = " ".join(i for i in data.Hashtags)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(12,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Likes", size="Likes", trendline="ols", 
+                    title = "Relationship Between Likes and Impressions")
+figure.show()
+
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Comments", size="Comments", trendline="ols", 
+                    title = "Relationship Between Comments and Total Impressions")
+figure.show()
+
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Shares", size="Shares", trendline="ols", 
+                    title = "Relationship Between Shares and Total Impressions")
+figure.show()
+
+correlation = data.corr()
+print(correlation["Impressions"].sort_values(ascending=False))
+
+
+conversion_rate = (data["Follows"].sum() / data["Profile Visits"].sum()) * 100
+print(conversion_rate)
+
+figure = px.scatter(data_frame = data, x="Profile Visits",
+                    y="Follows", size="Follows", trendline="ols", 
+                    title = "Relationship Between Profile Visits and Followers Gained")
+figure.show()
+
+
